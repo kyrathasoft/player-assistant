bc2e82749e743879d2af0921a3f67d51fb579d54
William.Miller <kyrathasoft@gmail.com>
2026-07-09 16:23:08 -0500

    commit Orcish lexicon updates made 7.8.2026

--- PlayerAssistant.Tests/obj/Release/net10.0-windows/PlayerAssistant.Tests.AssemblyInfo.cs
+++ PlayerAssistant.Tests/obj/Release/net10.0-windows/PlayerAssistant.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayerAssistant.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e28f258897bea4716f5f658dcd63960785033f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a53695c14a678d1588ceb749e091be88233e886")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayerAssistant.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayerAssistant.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
