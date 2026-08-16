d048783ca8e00dbe014043cd91cbee5778f0445d
William.Miller <kyrathasoft@gmail.com>
2026-08-16 13:17:33 -0500

    chore: commit reviewed PWA and broker updates

--- pwa/version.js
+++ pwa/version.js
@@ -3,7 +3,7 @@
 globalThis.PLAYER_ASSISTANT_VERSION_METADATA = Object.freeze({
     pwaVersion: '0.9.8',
     metadataRevision: 1,
-    stylesRevision: 45,
-    appRevision: 89,
-    cacheRevision: 107
+    stylesRevision: 43,
+    appRevision: 62,
+    cacheRevision: 76
 });
